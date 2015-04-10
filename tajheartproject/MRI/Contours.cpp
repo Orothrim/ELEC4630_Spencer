eb3d87ab0fe41cd44887cda3672cde0f926dcc42
Spencer Walker-Fooks <swfandstorm@hotmail.com>
2015-04-11 00:29:16 +1000

    Contours function is finished, run with the the command ./Contours.  It finds the heart, and the area of the heart.

--- tajheartproject/MRI/Contours.cpp
+++ tajheartproject/MRI/Contours.cpp
@@ -4,92 +4,112 @@
 #include "iostream"
 
 #define DEBUG 1
-
+#define SQUARE_SIZE 200
 
 using namespace cv;
 using namespace std;
 
 Point mousePos;
-int i;
+Point cropPos;
+char i;
+int key;
+int largestArea = 0;
+int contourIndex = 0;
 
-void onMouse(int event, int x, int y, int flags, void* userdata)
-{
-	if(event == EVENT_LBUTTONDOWN)
-	{
+void onMouse(int event, int x, int y, int flags, void* userdata) {
+	if(event == EVENT_LBUTTONDOWN) {
 		mousePos.x = x;
 		mousePos.y = y;
 	}
 }
 
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 	const char* filename = argc >= 2 ? argv[1] : "MRI1_01.png";
 
   Mat originalImage = imread(filename);
-  if(originalImage.empty())
-  {
+  if(originalImage.empty()) {
     cout << "can not open " << filename << endl;
     return -1;
   }
 	if (DEBUG) {cout << "File Loaded\n\r";}
 
+	cropPos.x = 292;
+	cropPos.y = 360;
+
 	int thres = 80;
-  Mat image = originalImage, pyr;
-  Mat edgeImage, contrastImage, blurImage;
-  Mat drawnImage = Mat::zeros(originalImage.size(), CV_8UC3);
+  Mat image = originalImage.clone(), pyr;
+  Mat edgeImage, contrastImage, blurImage, binaryImage, contourImage, croppedImage;
+
 
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
   RNG rng(12345);
 
-	// cout << "Would you like to center the search area? [y/n]\n\r";
-	// cin >> i;
-	// if(i=="y"){
-	// 	namedWindow("Click Image", WINDOW_NORMAL);
-	//   imshow("Click Image", image);
-	//   setMouseCallback("Click Image", onMouse, NULL)
-	//   cout << mousePos;
-	// }
+	cout << "Would you like to center the search area? [y/n]\n\r";
+	cin >> i;
+	if(i=='y') {
+		namedWindow("Click Image", WINDOW_AUTOSIZE);
+	  setMouseCallback("Click Image", onMouse, NULL);
+	  imshow("Click Image", image);
+	  waitKey(0);
+	  destroyWindow("Click Image");
+	  if(DEBUG) {cout << mousePos <<"\n\r";}
+	  cropPos = mousePos;
+	}
 
-  // pyrDown(image, pyr, Size(image.cols/2, image.rows/2));
-  // pyrUp(pyr, image, image.size());
-	image.convertTo(contrastImage, -1, 2.2, 10);
-  
+	croppedImage = image(Rect((cropPos.x-(SQUARE_SIZE*0.5)), (cropPos.y-(SQUARE_SIZE*0.5)), SQUARE_SIZE, SQUARE_SIZE));
+
+  Mat drawnImage = Mat::zeros(croppedImage.size(), CV_8UC3);
+
+  pyrDown(croppedImage, pyr, Size(croppedImage.cols/2, croppedImage.rows/2));
+  pyrUp(pyr, croppedImage, croppedImage.size());
+	croppedImage.convertTo(contrastImage, -1, 2.2, 10);
+
   Canny(contrastImage, edgeImage, thres, 2*thres, 3);
   blur(edgeImage, blurImage, Size(7,7));
 
-  
 	if (DEBUG) {cout << "Edge Image Created\n\r";}
 
-  //threshold(image, binaryImage, 50, 255, THRESH_BINARY);
+	croppedImage = blurImage.clone();
 
-  findContours(blurImage, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
+  findContours(croppedImage, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
 	if (DEBUG) {cout << "Contours Found\n\r";}
 
-  for(int i=0; i<contours.size(); i++)
-  {
-  	Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
-  	drawContours(drawnImage, contours, i, color, CV_FILLED, 8, hierarchy);
+  for(int i=0; i<contours.size(); i++) {
+		double area = contourArea(contours[i], false);
+		if(area > largestArea) {
+			largestArea = area;
+			contourIndex = i;
+		}
   }
 
-  if(DEBUG) 
-  {
-	  cout << "Contours Drawn\n\r";
-	  namedWindow("Original Image", WINDOW_NORMAL);
-	  imshow("Original Image", image); 
-	  namedWindow("Blur Image", WINDOW_NORMAL);
-	  imshow("Blur Image", blurImage); 
-	  namedWindow("Edge Image", WINDOW_NORMAL);
-	  imshow("Edge Image", edgeImage);
-  }
+ 	Scalar color = Scalar(255, 0, 0);
+  drawContours(drawnImage, contours, contourIndex, color, CV_FILLED, 8, hierarchy);
 
-  namedWindow("Contours Image", WINDOW_NORMAL);
-  imshow("Contours Image", drawnImage);
+	namedWindow("Contours Image", WINDOW_NORMAL);
+	imshow("Contours Image", drawnImage);
+	resizeWindow("Contours Image", 325, 400);
+	moveWindow("Contours Image", 330, 425);
+
+	if(DEBUG) {
+		cout << "Contours Drawn\n\r";
+		namedWindow("Original Image", WINDOW_NORMAL);
+		imshow("Original Image", originalImage);
+		resizeWindow("Original Image", 325, 400);
+		moveWindow("Original Image", 0, 0);
+
+		namedWindow("Edge Image", WINDOW_NORMAL);
+		imshow("Edge Image", edgeImage);
+		resizeWindow("Edge Image", 325, 400);
+		moveWindow("Edge Image", 330, 0);
+
+		namedWindow("Blur Image", WINDOW_NORMAL);
+		imshow("Blur Image", blurImage);
+		resizeWindow("Blur Image", 325, 400);
+		moveWindow("Blur Image", 0, 425);
+	}
+
+
   waitKey(0);
 }
-
-// double a = contourArea(controus[i], false);
-//   	if(a>largest_area){
-//   		largest_area = 
-//   	}
